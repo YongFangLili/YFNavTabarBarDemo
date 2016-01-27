7eaa466e950a024c4c996be74e6bd35d6787122b
YongFangLili <710215666@qq.com>
2016-01-27 14:01:30 +0800

    实现上下联动效果

--- YFNavTabarBarDemo/YFNavTabarBarDemo/YFNavTabarBar/Controller/YFNavTabarBarController.m
+++ YFNavTabarBarDemo/YFNavTabarBarDemo/YFNavTabarBar/Controller/YFNavTabarBarController.m
@@ -37,6 +37,8 @@
 //    self.titlesArray = @[@"标题1",@"标题2标题2标题2标题2标题2",@"标题3",@"标题4",@"标题5",@"标题6",@"标题7",@"标题8",@"标题9",];
     //初始化
     [self setUpbtns:self.titlesArray];
+    self.currentIndex = 0;
+    
     
 }
 
@@ -91,6 +93,9 @@
     NSInteger index = tap.view.tag - 1000;
     [self showLineWithButtonWidth:[self.itemsWidths[index] floatValue] withIndex:index];
     [self addPresentViewController:self.currentController WithIndex:index];
+    
+    
+    
 }
 
 #pragma mark -改变 contenoffset
@@ -105,6 +110,7 @@
     [self.mainView addSubview:[self.subViewControllers[index] view]];
     [VC addChildViewController:self];
     [VC.view addSubview:self.view];
+    [self setUpMainViewUI];
 
 }
 
@@ -130,13 +136,24 @@
     
     return widths;
 }
-#pragma mark -滑块的显示
+#pragma mark -滑块、以及视图的切换
 - (void)showLineWithButtonWidth:(CGFloat)width withIndex:(NSInteger)index;
 {
     CGFloat labelX = [self.LablxArray[index] floatValue];
+    // 获取右边下一个 labelX
+    CGFloat labelNextX = (index == self.titlesArray.count - 1) ? self.titleScroolView.contentSize.width : [self.LablxArray[index + 1] floatValue];
+    // 获取 labelx 对应的 label 的中心位置
+    CGFloat labelCenterx = (labelNextX - labelX) / 2.0f + labelX;
+    // 中心位置移动到屏幕的位置
+    CGFloat offsetX = labelCenterx * AutoSizeScaleX - KScreenWidth / 2.0f;
+    //当右侧移动到底的情况下
+    offsetX = offsetX > self.titleScroolView.contentSize.width - KScreenWidth ? self.titleScroolView.contentSize.width - KScreenWidth : offsetX;
+    //当左侧移动到底
+    offsetX = offsetX < 0 ? 0 : offsetX;
+    
     [UIView animateWithDuration:0.3 animations:^{
         //改变 cotentoffset
-        self.titleScroolView.contentOffset = CGPointMake(labelX, 0);
+        self.titleScroolView.contentOffset = CGPointMake(offsetX, 0);
 
     } completion:^(BOOL finished) {
         
@@ -145,15 +162,29 @@
         }];
         
     }];
+    self.mainView.contentOffset = CGPointMake(index * KScreenWidth, 0);
     
-    
-   
 }
 
 #pragma mark -添加 mainView 的内容
--(void)setUpMainViewUI {
+-(void)setUpMainViewUI:(NSInteger)index {
     
-    for (int i = 0; i<self.subViewControllers.count; i++) {
+    
+        UIViewController *VC = self.subViewControllers[index];
+        
+        VC.view.frame = CGRectMake(index * KScreenWidth,0, KScreenWidth, _mainView.frame.size.height);
+        
+        [self.mainView addSubview:VC.view];
+        
+        [self addChildViewController:VC];
+    
+}
+
+
+#pragma mark - 初始化子控制器
+-(void)setUpMainViewUI{
+    
+    for (int i = 0; i < self.subViewControllers.count; i++) {
         
         UIViewController *VC = self.subViewControllers[i];
         
@@ -162,21 +193,32 @@
         [self.mainView addSubview:VC.view];
         
         [self addChildViewController:VC];
+        
     }
     
 }
 
 
+
 #pragma mark -UIScrollviewDelegate
 -(void)scrollViewDidScroll:(UIScrollView *)scrollView{
-    
+
     //设置当前的 index
+   NSInteger currentIndex = (scrollView.contentOffset.x + KScreenWidth / 2.0f) / KScreenWidth;
     
-
-
-
+//    CGFloat X = (scrollView.contentOffset.x - currentIndex * KScreenWidth) + KScreenWidth / 2.0
+//    self.mainView.contentOffset = CGPointMake(currentIndex * KScreenWidth, 0);
+    
+//    [self showLineWithButtonWidth:[self.itemsWidths[currentIndex] floatValue] withIndex:currentIndex];
+    if (self.currentIndex != currentIndex) {
+        self.currentIndex = currentIndex;
+    }
+    
+    
 
 }
+
+
 
 #pragma mark - 懒加载
 -(UIScrollView *)titleScroolView{
@@ -186,12 +228,14 @@
         _titleScroolView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0,KScreenWidth , 44.0f * AutoSizeScaleY)];
         
         [self.view addSubview:_titleScroolView];
-        _titleScroolView.delegate = self;
+//        _titleScroolView.delegate = self;
+    
 //        _titleScroolView.pagingEnabled = YES;
         _titleScroolView.backgroundColor = [UIColor cyanColor];
         _titleScroolView.userInteractionEnabled = YES;
         _titleScroolView.bounces = NO;
-        _titleScroolView.showsVerticalScrollIndicator = NO;
+//        _titleScroolView.delegate = self;
+        _titleScroolView.showsHorizontalScrollIndicator = NO;
     }
     return _titleScroolView;
 }
@@ -238,12 +282,25 @@
     
     if(!_mainView){
         _mainView = [[UIScrollView alloc] initWithFrame:CGRectMake(0.0f, CGRectGetMaxY(self.titleScroolView.frame), KScreenWidth, KScreenHeight - CGRectGetMaxY(self.titleScroolView.frame))];
-        _mainView.contentSize = CGSizeMakeCustom(self.subViewControllers.count * KScreenWidth, 0);
+        _mainView.contentSize = CGSizeMake(self.subViewControllers.count * KScreenWidth, 0);
         _mainView.backgroundColor = [UIColor purpleColor];
         _mainView.delegate = self;
+        _mainView.bounces = NO;
+        _mainView.pagingEnabled = YES;
         [self.view addSubview:_mainView];
     }
     return _mainView;
+}
+
+-(void)setCurrentIndex:(NSInteger)currentIndex {
+
+    _currentIndex = currentIndex;
+    
+    [UIView animateWithDuration:0.1 animations:^{
+        [self showLineWithButtonWidth:[self.itemsWidths[currentIndex] floatValue] withIndex:currentIndex];
+    }];
+    
+   
 }
 
 @end
